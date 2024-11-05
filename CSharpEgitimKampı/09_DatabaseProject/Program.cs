39ac25566012c9e6440a28a3d86ccba5d331fb98
BETÜL DURGUN <157019812+BetulDURGUN@users.noreply.github.com>
2024-11-05 10:49:51 +0300

    Metotlar ve Database

---
+++ CSharpEgitimKampı/09_DatabaseProject/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_DatabaseProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ado.net
+            Console.WriteLine("***** C# Veri TabanlıÜrün Kategori Bilgi Sistemi*****");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string tableNumber;
+
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+
+            Console.WriteLine("Lütfen getirmek istediğiniz tablo numarasını giriniz. ");
+            tableNumber = Console.ReadLine();
+            Console.WriteLine("-----------------------------");
+
+            SqlConnection connection = new SqlConnection("Data Source=LAPTOP-IAJS6HRT;initial Catalog=EgitimKampiDb;integrated security=true");
+            connection.Open(); //BAĞLANTIYI OLUŞTURDUK VE AÇTIK.
+            SqlCommand command = new SqlCommand("Select * From TblCategory",connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();  
+            adapter.Fill(dataTable);
+            connection.Close();
+
+            foreach(DataRow row in dataTable.Rows)
+            {
+              foreach(var item in row.ItemArray)
+                {
+Console.Write(item.ToString());
+                }
+              Console.WriteLine();
+            }
+
+
+            Console.Read();
+        }
+    }
+}
